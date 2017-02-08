07d496c34432a6add19d90ef3389055c762fe44b
loverman85 <loverman85@hanmail.net>
2017-02-08 22:26:36 +0900

    뷰 맵핑완료

--- app.js
+++ app.js
@@ -2,20 +2,68 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var app = express();
-app.get('/',function(req,res){
-  res.send('hello main');
-});
+var ejs = require('ejs');
 
-app.get('/hi',function(req,res){
-  res.send('hello world');
-});
-
+// db Connection
 var conn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'java0000',
   database : 'library'
 });
+//bodyParser 사용설정
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//뷰파일들을 views_file폴더밑에 놓겠다고 설정
+app.set('views','./views');
+//express에게 ejs를 쓰겠다고 선언
+app.set('view engine','ejs');
+
+// root요청
+app.get('/',function(req, res){
+  res.send('hello main');
+});
+
+//로그인요청
+app.get('/login', function(req, res){
+  res.render('login', {});
+});
+
+//main요청
+app.get('/main', function(req, res){
+  res.render('main',{});
+});
+
+//도서추가
+app.get('/bookAdd', function(req, res){
+  res.render('bookAdd',{});
+});
+
+//도서폐기
+app.get('/bookDisposal', function(req, res){
+  res.render('bookDisposal',{});
+});
+
+//도서대여
+app.get('/bookRent', function(req, res){
+  res.render('bookRent',{});
+});
+
+//도서반납
+app.get('/bookReturn', function(req, res){
+  res.render('bookReturn',{});
+});
+
+//회원등록
+app.get('/memberAdd', function(req, res){
+  res.render('memberAdd',{});
+});
+
+app.get('/hi',function(req,res){
+  res.send('hello world');
+});
+
+
 
 
 conn.connect();
