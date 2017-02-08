aab4d22966bade0cbce1333d53499bf32c952ac4
loverman85 <loverman85@hanmail.net>
2017-02-08 23:24:22 +0900

    로그인, 로그아웃처리완료

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var app = express();
 var ejs = require('ejs');
+var session = require('express-session');
+
+app.use(session({
+  secret: 'H!@OJ$#ISNC@JRGO$J!@#',  // 쿠키에 저장할 connect.sid값을 암호화할 키값 입력
+  resave: false,  //세션 아이디를 접속할때마다 새롭게 발급하지 않음
+  saveUninitialized: true,  //세션 아이디를 실제 사용하기전에는 발급하지 않음
+}));
 
 // db Connection
 var conn = mysql.createConnection({
@@ -19,15 +26,57 @@ app.set('views','./views');
 //express에게 ejs를 쓰겠다고 선언
 app.set('view engine','ejs');
 
-// root요청
-app.get('/',function(req, res){
-  res.send('hello main');
-});
+
+
 
 //로그인요청
-app.get('/login', function(req, res){
+app.get('/', function(req, res){
   res.render('login', {});
 });
+//로그인실행
+app.post('/login', function(req, res){
+  var libraryId = req.body.libraryId;
+  var libraryPw = req.body.libraryPw;
+  console.log('libraryId:'+libraryId);
+  console.log('libraryPw:'+libraryPw);
+  var sql = 'SELECT library_id as libraryId FROM library WHERE library_id = ? AND library_pw = ?';
+  conn.query(sql, [libraryId,libraryPw], function(err, result, fields){
+    console.log('result:'+result[0].libraryId);
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      if(result[0].libraryId){
+        console.log('로그인성공');
+        req.session.id=libraryId;
+        //res.send(req.session.id);
+        console.log('session:'+req.session.id);
+        res.redirect('/main');
+      }
+    }
+    conn.end();
+  });
+});
+
+//로그아웃
+app.get('/logout', function(req, res){
+  console.log('session:'+req.session.id);
+  //세션삭제
+  if(req.session.id){
+    req.session.destroy(function(err){
+      if(err){
+        console.log(err);
+      } else {
+        res.redirect('/');
+      }
+    });
+  } else {
+    res.redirect('/');
+  }
+  console.log('session:'+req.session);
+});
+
+
 
 //main요청
 app.get('/main', function(req, res){
@@ -62,13 +111,6 @@ app.get('/memberAdd', function(req, res){
 app.get('/hi',function(req,res){
   res.send('hello world');
 });
-
-
-
-
-conn.connect();
-//이안에 쿼리문 작성
-conn.end();
 
 
 app.listen(3000,function(){
