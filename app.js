5a6c6e44839b10b7e9f45ffe0f08511d73033608
loverman85 <loverman85@hanmail.net>
2017-02-10 00:04:31 +0900

    회원등록 완료

--- app.js
+++ app.js
@@ -610,7 +610,7 @@ app.get('/libraryAdd', function(req, res){
       console.log(err);
       res.status(500).send('Internal Server Error');
     } else {
-      res.render('libraryAdd',{result});
+      res.render('libraryAdd',{result:result});
     }
   })
 });
@@ -659,11 +659,45 @@ app.post('/libraryAdd', function(req,res){
 
 
 
-//회원등록
+//회원등록 폼
 app.get('/memberAdd', function(req, res){
-  res.render('memberAdd',{});
+  let selectMemberLevelSql = `SELECT
+                          			memberlevel_no as memberLevelNo,
+                          			memberlevel_name as memberLevelName,
+                          			price
+                          		FROM memberlevel`;
+  conn.query(selectMemberLevelSql, function(err, result){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }else{
+      res.render('memberAdd',{level:result});
+    }
+  });
 });
 
+app.post('/memberAdd', function(req, res){
+  let memberName = req.body.memberName;
+  let memberPhone = req.body.memberPhone;
+  let memberLevelNo = req.body.memberLevelNo;
+
+  let insertMember = `INSERT INTO member(
+                  			member_name,
+                  			member_phone,
+                  			memberlevel_no
+                  		) VALUES (
+                  			?,?,?
+                  		)`;
+  conn.query(insertMember, [memberName, memberPhone, memberLevelNo], function(err, result){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }else{
+      console.log('회원등록 성공');
+      res.redirect('/memberAdd');
+    }
+  });
+});
 
 
 // 테스트용
