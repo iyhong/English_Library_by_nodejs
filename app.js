cdd5706c70c9e4c935813c444168b25d03298cf8
loverman85 <loverman85@hanmail.net>
2017-02-09 15:39:27 +0900

    transaction처리 완료

--- app.js
+++ app.js
@@ -256,137 +256,168 @@ app.post('/bookRent', function(req, res){
   }
   console.log('rentalEnd:'+rentalEnd);
 
-  //트랙잭션 시작
-  conn.beginTransaction(function(err){
-    if(err){throw err;}
 
-    //도서상태 조회후 도서상태가 1이 아니면 fail로 보냄
-    let selectBookStateSql = `SELECT
-                           			state_no as stateNo
-                           		FROM book
-                           		WHERE book_code = ?`;
-    conn.query(selectBookStateSql,[bookCode], function(err, result){
-      if(err){
-        console.log(err);
-        res.status(500).send('Internal Server Error');
+
+  //도서상태 조회후 도서상태가 1이 아니면 fail로 보냄
+  let selectBookStateSql = `SELECT
+                         			state_no as stateNo
+                         		FROM book
+                         		WHERE book_code = ?`;
+  conn.query(selectBookStateSql,[bookCode], function(err, result){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+
+      if(!result[0]){
+        console.log('bookCode:'+bookCode);
+        console.log('도서가없어요');
+        res.render('fail', {message:'없는도서입니다.'});
+        return;
+        //res.redirect('/fail');
+      } else if (result[0].stateNo===2){
+        console.log('bookCode:'+bookCode);
+        console.log('도서상태값:'+result[0].stateNo);
+        console.log('대여중인 도서입니다');
+        res.render('fail', {message:'대여중인 도서입니다'});
+        return;
+      } else if (result[0].stateNo===3){
+        console.log('도서상태값:'+result[0].stateNo);
+        console.log('폐기된 도서입니다');
+        res.render('fail', {message:'폐기된 도서입니다'});
+        return;
       } else {
-        //console.log('도서상태값:'+result[0].stateNo);
-        if(!result[0]){
-          console.log('도서가없어요');
-          res.render('fail', {message:'없는도서입니다.'});
-          //res.redirect('/fail');
-        } else if (result[0].stateNo===2){
-          console.log('대여중인 도서입니다');
-          res.render('fail', {message:'대여중인 도서입니다'});
-        } else if (result[0].stateNo===3){
-          console.log('폐기된 도서입니다');
-          res.render('fail', {message:'폐기된 도서입니다'});
-        } else {
-
-        }
-      }
-    });
-
-    //회원아이디 검색해보기 없으면 fail로
-    let selectMemberSql = `SELECT
-                      			member_id as memberId
-                      		FROM member
-                      		WHERE member_id = ?`;
-    conn.query(selectMemberSql, [memberId], function(err, result){
-      if(err){
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-      } else {
-        if(!result[0]){
-          res.render('fail',{message:'없는 회원입니다.'});
-        }
-      }
-    });
-
-    //autoincrement 가져오기
-    //db에서 autoincrement값 가져와서 formatting 하고 rentalCode에 합쳐준다.
-    let selectNoSql = `SELECT
-                  			max(auto_num) as num
-                  		FROM rental`;
-    conn.query(selectNoSql,function(err,result){
-      console.log('-----conn.query----');
-      if(err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-      } else {
-        console.log(result[0].num);
-        rentalCode += leadingZeros(result[0].num,5);
-        console.log('rentalCode:'+rentalCode);
-
-        //대여 등록
-        let insertRentalSql = `INSERT INTO rental(
-                          			rental_code,
-                          			book_code,
-                          			rental_start,
-                          			rental_end,
-                          			member_id,
-                          			rental_payment
-                          		) VALUES (
-                          			?,?,?,?,?,?
-                          		)`;
-        conn.query(insertRentalSql, [rentalCode, bookCode, rentalStart, rentalEnd, memberId, rentalPayment], function(err, result){
+        //회원아이디 검색해보기 없으면 fail로
+        let selectMemberSql = `SELECT
+                                member_id as memberId
+                              FROM member
+                              WHERE member_id = ?`;
+        conn.query(selectMemberSql, [memberId], function(err, result){
           if(err){
             console.log(err);
             res.status(500).send('Internal Server Error');
           } else {
-            console.log('대여등록 성공');
-          }
-        });
-
-        //도서상태 변경(대여가능->대여불가)
-        let updateBookStateSql = `UPDATE book SET
-                                    state_no = ?
-                                  WHERE book_code = ?`;
-        conn.query(updateBookStateSql,[2, bookCode], function(err, result){
-          if(err){
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-          } else {
-            console.log('도서상태 UPDATE');
-          }
-        });
-
-        //firstday select
-        //도서의 firstday 가져온다(null인지 아닌지 확인하기위해서)
-        let selectBookFirstDaySql = `SELECT
-                                      book_firstday as bookFirstday
-                                    FROM book
-                                    WHERE book_code=?`;
-        conn.query(selectBookFirstDaySql, [bookCode], function(err, result){
-          if(err){
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-          } else {
-            //도서의 firstday 가 null이면 오늘날짜를 update시켜준다.
-            if(!result[0].bookFirstday){
-              let updateBookFirstdaySql = `UPDATE book SET
-                                            book_firstday = sysdate()
-                                          WHERE book_code = ?`;
-              conn.query(updateBookFirstdaySql, [bookCode], function(err, result){
-                if(err){
-                  console.log(err);
-                  res.status(500).send('Internal Server Error');
-                } else {
-                  console.log('도서 firstday UPDATE');
-                  res.redirect('/bookRent');
-                }
-              });
-            } else {
-              //도서의 firstday 가 null이 아니면 끝
-              res.redirect('/bookRent');
+            if(!result[0]){
+              res.render('fail',{message:'없는 회원입니다.'});
+              return;
             }
+
+
+            //트랙잭션 시작
+            conn.beginTransaction(function(err){
+              if(err){throw err;}
+
+            //대여시작
+            //autoincrement 가져오기
+            //db에서 autoincrement값 가져와서 formatting 하고 rentalCode에 합쳐준다.
+            console.log('-----대여 프로세스 시작----');
+
+            let selectNoSql = `SELECT
+                          			max(auto_num) as num
+                          		FROM rental`;
+            conn.query(selectNoSql,function(err,result){
+              if(err) {
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+              } else {
+                console.log(result[0].num);
+                rentalCode += leadingZeros(result[0].num,5);
+                console.log('rentalCode:'+rentalCode);
+
+                //대여 등록
+                let insertRentalSql = `INSERT INTO rental(
+                                  			rental_code,
+                                  			book_code,
+                                  			rental_start,
+                                  			rental_end,
+                                  			member_id,
+                                  			rental_payment
+                                  		) VALUES (
+                                  			?,?,?,?,?,?
+                                  		)`;
+                conn.query(insertRentalSql, [rentalCode, bookCode, rentalStart, rentalEnd, memberId, rentalPayment], function(err, result){
+                  if(err){
+                    console.log(err);
+                    res.status(500).send('Internal Server Error');
+                  } else {
+                    console.log('대여등록 성공');
+                  }
+                });
+
+                //도서상태 변경(대여가능->대여불가)
+                let updateBookStateSql = `UPDATE book SET
+                                            state_no = ?
+                                          WHERE book_code = ?`;
+                conn.query(updateBookStateSql,[2, bookCode], function(err, result){
+                  if(err){
+                    console.log(err);
+                    res.status(500).send('Internal Server Error');
+                  } else {
+                    console.log('도서상태 UPDATE');
+                  }
+                });
+
+                //firstday select
+                //도서의 firstday 가져온다(null인지 아닌지 확인하기위해서)
+                let selectBookFirstDaySql = `SELECT
+                                              book_firstday as bookFirstday
+                                            FROM book
+                                            WHERE book_code=?`;
+                conn.query(selectBookFirstDaySql, [bookCode], function(err, result){
+                  if(err){
+                    console.log(err);
+                    res.status(500).send('Internal Server Error');
+                  } else {
+                    //도서의 firstday 가 null이면 오늘날짜를 update시켜준다.
+                    if(!result[0].bookFirstday){
+                      let updateBookFirstdaySql = `UPDATE book SET
+                                                    book_firstday = sysdate()
+                                                  WHERE book_code = ?`;
+                      conn.query(updateBookFirstdaySql, [bookCode], function(err, result){
+                        if(err){
+                          console.log(err);
+                          res.status(500).send('Internal Server Error');
+                        } else {
+                          console.log('도서 firstday UPDATE');
+                          conn.commit(function(err) {
+                            if (err) {
+                              conn.rollback(function() {
+                                throw err;
+                              });
+                            }
+                          });
+
+                          res.redirect('/bookRent');
+                        }
+                      });
+                    } else {
+                      //도서의 firstday 가 null이 아니면 끝
+                      conn.commit(function(err) {
+                        if (err) {
+                          conn.rollback(function() {
+                            throw err;
+                          });
+                        }
+                      });
+                      res.redirect('/bookRent');
+                    }
+                  }
+                });
+              }
+            });
+            console.log('-----대여 프로세스 끝----');
+            //대여종료
+          });
+          //트랜잭션 끝
           }
         });
+        //회원아이디 검색 끝
       }
-      console.log('-----conn.query----');
-    });
+      //도서가 대여가능상태인 else문 끝
+    }
+    //도서상태조회쿼리 error없는 else문 끝
   });
-  //트랜잭션 끝
+  //도서상태조회 끝
 });
 
 //도서반납
