f2031db115d29baed9a6d2318f5bc94c0f9a3186
loverman85 <loverman85@hanmail.net>
2017-02-09 23:50:59 +0900

    도서관등록 완료

--- app.js
+++ app.js
@@ -599,15 +599,71 @@ app.post('/getRental', function(req, res){
 });
 
 
+//도서관등록 폼
+app.get('/libraryAdd', function(req, res){
+  let selectLocalSql = `SELECT
+                    			local_no as localNo,
+                    			local_name as localName
+                    		FROM local`;
+  conn.query(selectLocalSql, function(err, result){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.render('libraryAdd',{result});
+    }
+  })
+});
+
+//도서관등록 실행
+app.post('/libraryAdd', function(req,res){
+  let libraryId = req.body.libraryId;
+  let libraryPw = req.body.libraryPw;
+  let local = req.body.local;
+
+  let selectLibrarySql =`SELECT
+                     			library_id as libraryId
+                    		FROM library
+                        WHERE library_id = ?`;
+  conn.query(selectLibrarySql, [libraryId], function(err, result){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      if(result[0]){
+        console.log('아이디 중복');
+        res.render('fail',{message: '아이디가 이미 있습니다.'});
+        return;
+      }
+      //도서관등록
+      let insertLibrarySql = `INSERT INTO library(
+                           			library_id,
+                           			library_pw,
+                           			local_no
+                           		)values(
+                           			?,?,?
+                           		)`;
+      conn.query(insertLibrarySql, [libraryId, libraryPw, local], function(err,result){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          console.log('도서관 등록 성공');
+          res.redirect('/');
+        }
+      });
+    }
+
+  });
+});
+
+
+
 //회원등록
 app.get('/memberAdd', function(req, res){
   res.render('memberAdd',{});
 });
 
-//도서관등록
-app.get('/memberAdd', function(req, res){
-  res.render('memberAdd',{});
-});
 
 
 // 테스트용
